8dcb4eb208cfab3b7a26a4704e92f6def101a749
cpprian <cpprian456@gmail.com>
2024-01-22 11:34:32 +0100

    fetch user tasks

--- frontend/app/(protected)/(routes)/profile/[profileId]/page.tsx
+++ frontend/app/(protected)/(routes)/profile/[profileId]/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 
 import { useQuery } from "@tanstack/react-query";
+import { fetchData } from "actions/api";
+import { convertTaskList } from "actions/tasks";
+import { Spinner } from "components/spinner";
 import { fetcher } from "lib/fetcher";
 import { User } from "next-auth";
+import { useEffect, useState } from "react";
+import { Task } from "schemas/task";
 
 interface ProfilePageProps {
     params: {
@@ -13,16 +18,58 @@ interface ProfilePageProps {
 const ProfilePage = ({
     params
 }: ProfilePageProps) => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [error, setError] = useState<number | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState(true);
     const { data: User } = useQuery<User>({
         queryKey: ["profile", params.profileId],
         queryFn: () => fetcher(`/api/profile/${params.profileId}`),
     })
+
+    const fetchUserTasks = async () => {
+        try {
+            const res = await fetchData(`/tasks/teacher/${params.profileId}`, "GET", {});
+            if (res?.status === 200) {
+                const data = await res.json();
+                setTasks(convertTaskList(data));
+            } else {
+                setError(res?.status);
+            }
+        } catch (err) {
+            setError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetchUserTasks();
+        setIsLoading(false);
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className="h-full flex flex-col items-center justify-center space-y-4">
+                <Spinner size="icon" />
+                <h1>Loading profile data...</h1>
+            </div>
+        )
+    }
     
     return (
         <div>
             <h1>Profile page</h1>
             <h2>{User?.email}</h2>
             <h2>{User?.name}</h2>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.ID}>
+                        <h3>{task.Title}</h3>
+                        <p>{task.Description}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 };
